3bd931a540e7f052942b4f25cb2579e9b1a767b3
dsa069 <dsa069@inlumine.ual.es>
2026-04-10 13:34:14 +0000

    chore: add Cypress configuration and update Karma config for improved testing setup

---
+++ cypress.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'cypress';
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:8100',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  component: {
+    devServer: {
+      framework: 'angular',
+      bundler: 'webpack',
+      options: {
+        projectConfig: {
+          root: './',
+          sourceRoot: 'src',
+          buildOptions: {
+            outputPath: 'dist/browser',
+          },
+        },
+      },
+    },
+    specPattern: '**/*.cy.ts',
+  },
+});
