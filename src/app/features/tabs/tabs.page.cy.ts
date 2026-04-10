9874b2920c7cea068ce252497af0e818a1ba8db5
dsa069 <dsa069@inlumine.ual.es>
2026-04-10 20:33:24 +0000

    test: add unit tests for TabsPage to verify tab rendering and navigation attributes

--- src/app/features/tabs/tabs.page.cy.ts
+++ src/app/features/tabs/tabs.page.cy.ts
@@ -1,13 +1,35 @@
 import { mount } from '@cypress/angular';
 import { TabsPage } from './tabs.page';
-import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
+import {
+  IonTabs,
+  IonTabBar,
+  IonTabButton,
+  IonIcon,
+  IonLabel,
+} from '@ionic/angular/standalone';
 
 describe('TabsPage Component', () => {
+  it('should render tab bar', () => {
+    mount(TabsPage, {
+      imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+    });
+
+    cy.get('ion-tab-bar').should('exist');
+  });
+
+  it('should render exactly three tab buttons', () => {
+    mount(TabsPage, {
+      imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+    });
+
+    cy.get('ion-tab-button').should('have.length', 3);
+  });
+
   it('should render all tab buttons with correct icons and labels', () => {
     mount(TabsPage, {
       imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
     });
-    // Espera a que los tabs se rendericen
+
     cy.get('ion-tab-bar').should('exist');
     cy.get('ion-tab-button[tab="tabClients"]').within(() => {
       cy.get('ion-icon[name="people"]').should('exist');
@@ -21,5 +43,59 @@ describe('TabsPage Component', () => {
       cy.get('ion-icon[name="pricetags"]').should('exist');
       cy.contains('Products');
     });
+  });
+
+  it('should render Clients tab with people icon', () => {
+    mount(TabsPage, {
+      imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+    });
+
+    cy.get('ion-tab-button[tab="tabClients"]').within(() => {
+      cy.get('ion-icon[name="people"]').should('exist').and('be.visible');
+    });
+  });
+
+  it('should render Reservations tab with calendar-number icon', () => {
+    mount(TabsPage, {
+      imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+    });
+
+    cy.get('ion-tab-button[tab="tabReservations"]').within(() => {
+      cy.get('ion-icon[name="calendar-number"]')
+        .should('exist')
+        .and('be.visible');
+    });
+  });
+
+  it('should render Products tab with pricetags icon', () => {
+    mount(TabsPage, {
+      imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+    });
+
+    cy.get('ion-tab-button[tab="tabProducts"]').within(() => {
+      cy.get('ion-icon[name="pricetags"]').should('exist').and('be.visible');
+    });
+  });
+
+  it('should have correct href attributes for navigation', () => {
+    mount(TabsPage, {
+      imports: [IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel],
+    });
+
+    cy.get('ion-tab-button[tab="tabClients"]').should(
+      'have.attr',
+      'href',
+      '/tabs/tabClients',
+    );
+    cy.get('ion-tab-button[tab="tabReservations"]').should(
+      'have.attr',
+      'href',
+      '/tabs/tabReservations',
+    );
+    cy.get('ion-tab-button[tab="tabProducts"]').should(
+      'have.attr',
+      'href',
+      '/tabs/tabProducts',
+    );
   });
 });
