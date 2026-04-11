1b0d9e6b6268a58f53be42a361f668244c61f7a8
dsa069 <dsa069@inlumine.ual.es>
2026-04-11 20:29:34 +0000

    fix: add withHashLocation to router configuration

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,7 @@ bootstrapApplication(AppComponent, {
       routes,
       withPreloading(PreloadAllModules),
       withComponentInputBinding(),
+      //  todo lo que está después del # no es enviado al servidor, sino que es manejado solo por el navegador y el framework.
       withHashLocation(),
     ),
   ],
